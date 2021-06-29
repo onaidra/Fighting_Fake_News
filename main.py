8a930f7f729b4b7b9e78f132e45ed5b0f95d4df6
onaidra <adriano.puglisi.193@gmail.com>
2021-06-29 16:37:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if solver.net.use_tf_threading:
     tf.train.start_queue_runners(sess=solver.sess, coord=solver.coord)
 
 im1 = util.random_crop(im1,[128,128])
-im1_merge = {'im_a':[im1,im1,im1,im1,im1,im1],'im_b':[im1,im1,im1,im1,im1,im1],'exif_lbl': ['a','b','c','d','e'],'cls_lbl': ['a','b','c','d','e']}
+im1_merge = {'im_a':[im1,im1,im1,im1,im1,im1],'im_b':[im1,im1,im1,im1,im1,im1],'exif_lbl': ['a','b','c','d','e'],'cls_lbl': ['1','0','1','0','0']}
 exif_solver.ExifSolver.setup_data(solver,im1,im1_merge)
 exif_solver.ExifSolver.train(solver)
 """
