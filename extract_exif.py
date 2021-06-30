51668c2aaa1a272d1680705ef59b84d7e0d08bbf
onaidra <adriano.puglisi.193@gmail.com>
2021-06-30 18:05:42 +0200

    files

--- extract_exif.py
+++ extract_exif.py
@@ -1,12 +1,17 @@
+from models import exif
 from PIL import Image
 from PIL.ExifTags import TAGS
 import os
+import cv2
+import random
+from lib.utils import benchmark_utils, util,io
+import numpy as np
 wrong_tags =[545,546,547,548,544,549]
 right_tags = ["ISO2","StopsAboveBaseISO","ExposureCompensation","BrightnessValue","FirmwareVersion","Info"]
 # path to the image or video
+
 def extract_exif():
-    path = os.getcwd()
-    path = os.path.join(path,"foto")
+    path = r"C:\Users\Adri\Desktop\VISIOPE\prova\foto"
     dir = os.listdir(path)
 
     right_dir = []
@@ -16,6 +21,7 @@ def extract_exif():
             s1 = os.path.join(s1,"orig")
             right_dir.append(s1)
     dict = {}
+    image_list = []
     index = 0
     for dir in right_dir:
         directory = os.listdir(dir)
@@ -25,6 +31,7 @@ def extract_exif():
             # read the image data using PIL
             image = Image.open(new_dir)
 
+            if new_dir not in image_list : image_list.append(new_dir)
             # extract EXIF data
             exifdata = image.getexif()
             
@@ -51,7 +58,84 @@ def extract_exif():
     for i in range(len(wrong_tags)):
         x = dict.pop(wrong_tags[i])
         dict[right_tags[i]] = x
-    return dict
+
+    print("[INFO] Extracted dict")
+    return dict,image_list
+
+def random_list(list):
+    second_list = []
+    for i in range(len(list)):
+        if i % 300 == 0:
+            second_list.append(list[i])
+        else:
+            second_list.append(random.choice(list))
+    print("[INFO] Generated second list")
+    return second_list
+
+def generate_label(first,second):
+    exif_lbl = []
+    if(len(first)!=len(second)):
+        print("list len do not match")
+        return
+    else:
+        for i in range(len(first)):
+            im1 = Image.open(first[i])
+            im2 = Image.open(second[i])
+            exif1 = im1.getexif()
+            exif2 = im2.getexif()
+            list_tag1={}
+            list_tag2={}
+            for tag_id in exif1:
+                # get the tag name, instead of human unreadable tag id
+                tag = TAGS.get(tag_id, tag_id)
+                data = exif1.get(tag_id)
+                if isinstance(data, bytes):
+                    data = data.decode()
+                data = str(data).strip(" ")
+                list_tag1[tag] = data
+            
+            for tag_id in exif2:
+                # get the tag name, instead of human unreadable tag id
+                tag = TAGS.get(tag_id, tag_id)
+                data = exif1.get(tag_id)
+                if isinstance(data, bytes):
+                    data = data.decode()
+                data = str(data).strip(" ")
+                list_tag2[tag] = data
+            shared_tags = []
+            
+            for elem in list_tag1.keys():
+                if elem in list_tag2.keys():
+                    if(list_tag1[elem] == list_tag2[elem]):
+                        shared_tags.append(1)
+                    else:
+                        shared_tags.append(0)
+                else:
+                    shared_tags.append(0)
+            
+            for elem in list_tag2.keys():
+                if elem not in list_tag1.keys():
+                    shared_tags.append(0)
+            
+            exif_lbl.append(shared_tags)
+        print("[INFO] Label extracted")
+        return exif_lbl
+    
+def cropping_list(first,second):
+    tmp1 = []
+    tmp2 = []
+    
+    for i in range(len(first)):
+        print(i)
+        x = cv2.imread(first[i])[:,:,[2,1,0]]
+        y = cv2.imread(second[i])[:,:,[2,1,0]]
+        im1 = util.random_crop(x,[128,128])
+        im2 = util.random_crop(y,[128,128])
+        tmp1.append(im1)
+        tmp2.append(im2)
+    
+    print("[INFO] Images cropped")
+    return tmp1,tmp2
 
 """
 ####################################################ORIGINAL #################################################
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from models import exif
+import random
 from models.exif import exif_solver,exif_net
 from load_models import initialize_exif
-from extract_exif import extract_exif
+from extract_exif import extract_exif, random_list,generate_label,cropping_list
 from lib.utils import benchmark_utils, util,io
 from demo import Demo
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import cv2
-#dict = extract_exif()
-#salvati = dict.keys()
-#print(salvati)
-im1 = cv2.imread("D01_img_orig_0001.jpg")[:,:,[2,1,0]]
+dict,image_list = extract_exif()
+salvati = list(dict.keys())
+key_chosen = random.choice(salvati)
+value_chosen = random.choice(dict[key_chosen])
+
+second_image_list = random_list(image_list)
+exif_lbl = generate_label(image_list,second_image_list)
+
+list1,list2 = cropping_list(image_list,second_image_list)
 print("---------------------------------------------------------------------")
-print(im1.shape)
 print("---------------------------------------------------------------------")
+
 solver = initialize_exif()
 solver.sess.run(tf.compat.v1.global_variables_initializer())
 if solver.net.use_tf_threading:
@@ -22,14 +28,14 @@ if solver.net.use_tf_threading:
     solver.net.train_runner.start_p_threads(solver.sess)
     tf.train.start_queue_runners(sess=solver.sess, coord=solver.coord)
 
-im1 = util.random_crop(im1,[128,128])
-exif_lbl = np.random.randint(0,2,(1,83))
-
 cls_lbl = np.ones((1,1))
+cls_lbl[0][0] = len(dict.keys())
 
-im1_merge = {'im_a':[im1,im1,im1,im1,im1,im1],'im_b':[im1,im1,im1,im1,im1,im1],'exif_lbl': exif_lbl,'cls_lbl': cls_lbl}
-exif_solver.ExifSolver.setup_data(solver,im1,im1_merge)
+
+im1_merge = {'im_a':list1,'im_b':list2,'exif_lbl': exif_lbl,'cls_lbl': cls_lbl}
+exif_solver.ExifSolver.setup_data(solver,list1,im1_merge)
 exif_solver.ExifSolver.train(solver)
+
 """
 im = np.zeros((256, 256, 3))
 
