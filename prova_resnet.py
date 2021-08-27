bf447f97284889c5f70b7a35b9df515a274f16da
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-08-27 11:50:35 +0200

    Update prova_resnet.py

--- prova_resnet.py
+++ prova_resnet.py
@@ -2,7 +2,8 @@ from models import exif
 from PIL import Image
 import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import ResNet50
-from tensorflow.keras import Input,Model
+from tensorflow.keras import Input
+from keras import Model
 from keras.layer import Dense,Flatten,Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import load_model
