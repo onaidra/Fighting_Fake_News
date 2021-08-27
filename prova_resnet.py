f464ff962b01bb22e89e5e704bfff790cbf6d492
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-08-27 12:29:45 +0200

    Update prova_resnet.py

--- prova_resnet.py
+++ prova_resnet.py
@@ -129,10 +129,7 @@ Y_train = list2
 cls_lbl= len(exif_lbl)
 training_generator = DataGenerator([X_train,Y_train], exif_lbl,n_classes=cls_lbl)
 
-siamese_model.fit_generator(generator=training_generator,
-                            steps_per_epoch=1000,
-                            epochs=10,
-                            verbose=1)
+siamese_model.fit_generator(generator=training_generator,steps_per_epoch=1000,epochs=10)
                             #callbacks=[checkpoint, tensor_board_callback, lr_reducer, early_stopper, csv_logger],
                             #validation_data=validation_data,
 
