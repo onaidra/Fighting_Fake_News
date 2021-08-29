747d3f573451ec84d211f67ae543cb0d2c0c6b31
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-08-29 18:24:01 +0200

    Update prova_resnet.py

--- prova_resnet.py
+++ prova_resnet.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras import Input
 from keras import Model
-from keras.layers import Dense,Flatten,Dropout
+from keras.layers import Dense,Flatten,Dropout,Lambda,RandomNormal
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import load_model
 from extract_exif import extract_exif, random_list,generate_label,cropping_list,get_np_arrays
@@ -12,84 +12,26 @@ import pickle
 
 import numpy as np
 
-
-class DataGenerator(tf.keras.utils.Sequence):
-    'Generates data for Keras'
-    def __init__(self, list_IDs, labels, batch_size=32, dim=(128,128,3), n_channels=1,
-                 n_classes=10, shuffle=True):
-        'Initialization'
-        self.dim = dim
-        self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_IDs
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.shuffle = shuffle
-        self.on_epoch_end()
-
-    def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
-
-    def __getitem__(self, index):
-        'Generate one batch of data'
-        # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-
-        # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
-
-        return X, y
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
-        # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size), dtype=int)
-
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            # Store sample
-            X[i,] = np.load('data/' + ID + '.npy')
-
-            # Store class
-            y[i] = self.labels[ID]
-
-        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
-
-
 def create_base_model(image_shape, dropout_rate, suffix=''):
     I1 = Input(shape=image_shape)
     model = ResNet50(include_top=False, weights='imagenet', input_tensor=I1, pooling=None)
     model.layers.pop()
     model.outputs = [model.layers[-1].output]
-    model.layers[-1]._outbound_nodes = []
+    model.layers[-1].outbound_nodes = []
 
     for layer in model.layers:
-        layer._name = layer.name + str(suffix)
-        layer._trainable = False
+        layer.name = layer.name + str(suffix)
+        layer.trainable = False
 
     flatten_name = 'flatten' + str(suffix)
 
     x = model.output
-    
     x = Flatten(name=flatten_name)(x)
-    
-    x = Dense(4096, activation='relu')(x)
-    """
+    x = Dense(1024, activation='relu')(x)
     x = Dropout(dropout_rate)(x)
     x = Dense(512, activation='relu')(x)
     x = Dropout(dropout_rate)(x)
-"""
+
     return x, model.input
 
 
@@ -98,54 +40,47 @@ def create_siamese_model(image_shape, dropout_rate):
     output_left, input_left = create_base_model(image_shape, dropout_rate)
     output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
 
-    L1_prediction = [output_left,output_right]
+    L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
+    L1_distance = L1_layer([output_left, output_right])
+    L1_prediction = Dense(1, use_bias=True,
+                          activation='sigmoid',
+                          kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
+                          name='weighted-average')(L1_distance)
 
-    #L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
-    #L1_distance = L1_layer([output_left, output_right])
-    #L1_prediction = Dense(1, use_bias=True,
-    #                      activation='sigmoid',
-    #                      kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
-    #                      name='weighted-average')(L1_distance)
+    prediction = Dropout(0.2)(L1_prediction)
 
-    siamese_model = Model(inputs=[input_left, input_right], outputs=L1_prediction)
+    siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
 
     return siamese_model
 
-
-
-siamese_model = create_siamese_model(image_shape=(128, 128, 3),dropout_rate=0.2)
+siamese_model = create_siamese_model(image_shape=(64, 64, 3),
+                                         dropout_rate=0.2)
 
 siamese_model.compile(loss='binary_crossentropy',
                       optimizer=Adam(lr=0.0001),
                       metrics=['binary_crossentropy', 'acc'])
-
-with open("exif_lbl.txt", "rb") as fp:   #Picklingpickle.dump(l, fp)
-	exif_lbl = pickle.load(fp)
-fp.close()
-
-list1,list2 = get_np_arrays('cropped_arrays.npy')
-X_train = list1
-Y_train = list2
-cls_lbl= len(exif_lbl)
-training_generator = DataGenerator([X_train,Y_train], exif_lbl,n_classes=cls_lbl)
-
-siamese_model.fit_generator(generator=training_generator,steps_per_epoch=1000,epochs=10,validation_data=training_generator)
+siamese_model.fit_generator("https://drive.google.com/drive/u/0/folders/1qtAc7_R9DVIf8-fqI5BqTQ8yC2nRbPgq/D01_img_orig_0143",
+                            steps_per_epoch=1000,
+                            epochs=10,
+                            verbose=1)
                             #callbacks=[checkpoint, tensor_board_callback, lr_reducer, early_stopper, csv_logger],
                             #validation_data=validation_data,
+                            #max_q_size=3)
 
 siamese_model.save('siamese_model.h5')
 
 
-
+"""
 # and the my prediction
 siamese_net = load_model('siamese_model.h5', custom_objects={"tf": tf})
-"""
 X_1 = [image, ] * len(markers)
 batch = [markers, X_1]
 result = siamese_net.predict_on_batch(batch)
+
 
 # I've tried also to check identical images 
 markers = [image]
 X_1 = [image, ] * len(markers)
 batch = [markers, X_1]
-result = siamese_net.predict_on_batch(batch)"""
+result = siamese_net.predict_on_batch(batch)
+"""
