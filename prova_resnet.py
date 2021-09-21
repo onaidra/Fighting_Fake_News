159325665dcb7c3eb097c79ac02a5a43646a11d3
AdrianoPuglisi <puglisi.1743285@studenti.uniroma1.it>
2021-09-21 18:27:55 +0200

    Update prova_resnet.py

--- prova_resnet.py
+++ prova_resnet.py
@@ -131,7 +131,7 @@ siamese_model = create_siamese_model(image_shape=(128,128, 3),
 
 siamese_model.compile(loss='categorical_crossentropy',
                       optimizer=Adam(lr=0.0001),
-                      metrics=['categorical_crossentropy', 'acc'])
+                      metrics=['accuracy'])
 
 with open("exif_lbl.txt", "rb") as fp:   #Picklingpickle.dump(l, fp)
 	exif_lbl = pickle.load(fp)
