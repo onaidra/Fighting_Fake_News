fbc27281cfbffd37db3037fa7c799cbe1910e0d4
onaidra <adriano.puglisi.193@gmail.com>
2021-09-22 10:42:57 +0200

    Update extract_exif.py

--- extract_exif.py
+++ extract_exif.py
@@ -64,8 +64,12 @@ def extract_exif():
     for key in dict:
         print("KEY : ", key)
         for i in range(len(dict[key])):
-            print("VALUE : ",dict[key][i][0]," len: ",dict[key][i][1])
-
+            if(len(dict[key][i][1])<30):
+                dict[key].pop(i)
+    
+    for k in dict:
+        print("key: ",k," len: ",len(dict[k]))
+        
     print("[INFO] Extracted dict")
     return dict,image_list,list(dict.keys())
 
