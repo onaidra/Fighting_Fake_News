97a65f7bc5f75f94743b4cdad006adae86067479
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-10-04 17:04:44 +0200

    Update prova_resnet_2.py

--- prova_resnet_2.py
+++ prova_resnet_2.py
@@ -80,7 +80,7 @@ def create_siamese_model(image_shape, dropout_rate):
     num_classes=45
     output_siamese = tf.square(output_siamese)
     x = Sequential()
-    x.add(Dense(output_siamese, activation='relu'))
+    x.add(Dense(output_siamese))
     x.add(Dense(4096, activation='relu'))
     x.add(Dense(2048, activation='relu'))
     x.add(Dense(1024, activation='relu'))
