03f89dcd2f4aad2dd45812af6851f42dbc939cfb
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-10-04 17:01:11 +0200

    Update prova_resnet_2.py

--- prova_resnet_2.py
+++ prova_resnet_2.py
@@ -75,7 +75,7 @@ def create_siamese_model(image_shape, dropout_rate):
     output_left, input_left = create_base_model(image_shape, dropout_rate)
     output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
     
-    print(type(output_left))
+    print(type(output_left.numpy()))
     print(type(output_right))
     
     output_siamese = tf.concat([output_left,output_right],1)
