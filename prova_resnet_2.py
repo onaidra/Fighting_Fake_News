6c921c566f1f9ca6e693a0628256ed28f926aec1
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-10-05 16:44:46 +0200

    Update prova_resnet_2.py

--- prova_resnet_2.py
+++ prova_resnet_2.py
@@ -73,10 +73,10 @@ def create_siamese_model(image_shape, dropout_rate):
     num_classes=45
     
     x = output_siamese
-    #x = Dense(4096, activation='relu')(x)
-    #x = Dense(2048, activation='relu')(x)
-    #x = Dense(1024, activation='relu')(x)
-    #x = Dense(num_classes, activation='softmax')(x)
+    x = Dense(4096, activation='relu')(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dense(num_classes, activation='softmax')(x)
     
     
     #model.summary()
@@ -84,7 +84,7 @@ def create_siamese_model(image_shape, dropout_rate):
     #out = model.output
     #sm_model = Model(inputs=[input_left, input_right], outputs=out)
     return x,input_left,input_right
-    
+"""   
 def create_mlp_model(output_siamese_shape):
 
     num_classes=45
@@ -105,15 +105,15 @@ def create_mlp_model(output_siamese_shape):
     out = model2.output
     
     return model2.input,out
-    
+   """ 
 def create_mlp(image_shape,dropout_rate):
     x,input_left,input_right = create_siamese_model(image_shape,
                                       dropout_rate)
     
-    print(x.shape)
-    input_mlp,output_mlp= create_mlp_model(x.shape)
+    
+    #input_mlp,output_mlp= create_mlp_model(x.shape)
     #output_siamese=Input(output_siamese_shape)
-    sm_model = Model(inputs=[input_left, input_right], outputs=output_mlp)
+    sm_model = Model(inputs=[input_left, input_right], outputs=x)
     
     return sm_model
     
