1dc4e09660165fe037ea298bc970d0e3867a1a53
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-10-05 16:54:47 +0200

    sadasfasDF

--- prova_resnet.py
+++ prova_resnet.py
@@ -61,18 +61,18 @@ def create_siamese_model(image_shape, dropout_rate):
     output_left, input_left = create_base_model(image_shape, dropout_rate)
     output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
     output = tf.concat([output_left,output_right],1)
-    """
-    L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
-    L1_distance = L1_layer([output_left, output_right])
+    
+    #L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
+    #L1_distance = L1_layer([output_left, output_right])
     L1_prediction = Dense(1, use_bias=True,
                           activation='sigmoid',
                           input_shape = image_shape,
                           kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
-                          name='weighted-average')(L1_distance)
+                          name='weighted-average')(output)
 
-    prediction = Dropout(0.2)(L1_prediction)"""
+    prediction = Dropout(0.2)(L1_prediction)
 
-    siamese_model = Model(inputs=[input_left, input_right], outputs=output)
+    siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
 
     return siamese_model
 
