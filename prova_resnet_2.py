45d4abdf779ddcbd143fb9a448d871c6731bab1a
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-10-06 15:42:39 +0200

    Update prova_resnet_2.py

--- prova_resnet_2.py
+++ prova_resnet_2.py
@@ -162,7 +162,7 @@ exif_lbl2 = exif_lbl[train_set:]
 x_train = datagenerator(list1_train,list2_train,exif_lbl,32)
 x_test = datagenerator(list1_test,list2_test,exif_lbl,32)
 #steps = len(list1)/EPOCHS
-steps = train_set/EPOCHS
+steps = int(train_set/EPOCHS)
 
 # imagexs = np.expand_dims(list1[0],axis=0)
 # imagexs2 = np.expand_dims(list2[0],axis=0)
