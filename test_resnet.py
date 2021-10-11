3202a43869aa55bb9b25250c8488f690500b331c
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-10-11 18:21:18 +0200

    Update test_resnet.py

--- test_resnet.py
+++ test_resnet.py
@@ -1,1 +1,16 @@
+from re import I
+from models import exif
+from PIL import Image
+from prova_resnet import EPOCHS
+import tensorflow as tf
+from extract_exif import extract_exif,generate_label,cropping_list,get_np_arrays,remove_elements,create_batch_samples
+from matplotlib import image
+from lib.utils import benchmark_utils, util,io
+import cv2
+import os
+import numpy as np
+import keras
+import pickle
+from keras.engine import keras_tensor
+
 print("CAZZO")
