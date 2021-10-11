9f4ef6287e3332b21d88d24d487c8ba66bd2caca
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-10-11 17:16:26 +0200

    ssa

--- test.py
+++ test.py
@@ -14,15 +14,15 @@ from keras.engine import keras_tensor
 
 import matplotlib.pyplot as plt
 
-def datagenerator(images,images2, batchsize, mode="train"):
+def datagenerator(images,images2,labels, batchsize, mode="train"):
     while True:
         start = 0
         end = batchsize
         while start  < len(images):
             x = images[start:end] 
-            #y = labels[start:end]
+            y = labels[start:end]
             x2 = images2[start:end]
-            yield (x,x2)
+            yield (x,x2),y
 
             start += batchsize
             end += batchsize
@@ -56,35 +56,13 @@ list1,list2 = cropping_list(list1_img,list2_img)
 #--------------------------------------------------------------- RUN MODEL
 x_train = datagenerator(list1,list2,exif_lbl,32)
 """
-#prova
-path = r"/content/drive/MyDrive/foto/test/images/2217.jpg"
-#dir = os.listdir(path)
-foto1 = cv2.imread(path)[:,:,[2,1,0]]
-foto2 = cv2.imread(path)[:,:,[2,1,0]]
-patch1 = util.random_crop(foto1,[128,128])
-patch2 = util.random_crop(foto2,[128,128])
-print(foto1.shape)
-plt.imshow(patch1)
-plt.imshow(patch2)
-"""
-#length = len(dir)
-tmp1 = np.empty((length, 128, 128, 3), dtype=np.uint8)
-tmp2 = np.empty((length, 128, 128, 3), dtype=np.uint8)
-i = 0
-for elem in dir:
-    elem = os.path.join(path,elem)
-    foto1 = cv2.imread(elem)[:,:,[2,1,0]]
-    foto2 = cv2.imread(elem)[:,:,[2,1,0]]
-    patch1 = util.random_crop(foto1,[128,128])
-    patch2 = util.random_crop(foto2,[128,128])
-    tmp1[i] = patch1
-    tmp2[i] = patch2
-    i+=1
-
+with open("exif_lbl.txt", "rb") as fp:   #Picklingpickle.dump(l, fp)
+	exif_lbl = pickle.load(fp)
+fp.close()
+list1,list2 = get_np_arrays('cropped_arrays.npy')
+x_train = datagenerator(list1,list2,exif_lbl,32)
 model = tf.keras.models.load_model('siameseMLP.h5')
 
+x = model.evaluate(x_train)
 
-x = model.predict((tmp1,tmp2))
-print(x)
 print(model.metrics_names)
-"""
