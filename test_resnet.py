e8ef3cf2128c92e7859797f4f05c548bebdf382f
AdrianoPuglisi <adriano.puglisi.193@gmail.com>
2021-10-12 11:24:39 +0200

    Update test_resnet.py

--- test_resnet.py
+++ test_resnet.py
@@ -82,6 +82,6 @@ x_train = datagenerator(tmp1,tmp2,32)
 
 model = tf.keras.models.load_model('siameseMLP.h5')
 print("[INFO] Starting Evaluation")
-model.evaluate(x_train,batch_size = 32)
+model.evaluate(x_train)
 
 print(model.metrics_names)
